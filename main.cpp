f47274513af288a403366b771b3fcf33d2ea4f45
hani <zaraki2011@gmail.com>
2026-04-12 04:44:02 +0300

    finish inputMouse

--- main.cpp
+++ main.cpp
@@ -9,7 +9,9 @@ int main(){
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
     int currentFps{60};
 
-    Vector2 ballPosition = {(float)screenWidth/2, (float)screenHeight/2};
+    Vector2 ballPosition = {-100.0F, -100.0F};
+    Color ballColor = DARKBLUE;
+
     SetTargetFPS(currentFps); //Detect window close button or ESC key
 
     //main game loop
@@ -17,19 +19,30 @@ int main(){
         //Update
         // ---------------
         // TODO: update your variable here
-        if(IsKeyPressed(KEY_RIGHT)) ballPosition.x += 2.0F;
-        if(IsKeyPressed(KEY_LEFT)) ballPosition.x -= 2.0F;
-        if(IsKeyPressed(KEY_UP)) ballPosition.y -= 2.0F;
-        if(IsKeyPressed(KEY_DOWN)) ballPosition.y += 2.0F;
+        // ----------------------------
+        if(IsKeyPressed(KEY_H)){
+            if(IsCursorHidden()) ShowCursor();
+            else HideCursor();
+        }
+        ballPosition = GetMousePosition();
 
-
-        // --------------------
+        if(IsMouseButtonDown(MOUSE_BUTTON_LEFT)) ballColor = MAROON;
+        else if(IsMouseButtonDown(MOUSE_BUTTON_MIDDLE)) ballColor = LIME;
+        else if(IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) ballColor = DARKBLUE;
+        else if(IsMouseButtonDown(MOUSE_BUTTON_SIDE)) ballColor = PURPLE;
+        else if(IsMouseButtonDown(MOUSE_BUTTON_EXTRA)) ballColor = YELLOW;
+        else if(IsMouseButtonDown(MOUSE_BUTTON_FORWARD)) ballColor = ORANGE;
+        else if(IsMouseButtonDown(MOUSE_BUTTON_BACK)) ballColor = BEIGE;
         // Draw
         // ---------------------
         BeginDrawing();
             ClearBackground(RAYWHITE);
-            DrawText("move the ball with arrow keys", 10, 10, 20, DARKGRAY);
-            DrawCircleV(ballPosition, 50, MAROON);
+            DrawCircleV(ballPosition, 40, ballColor);
+            DrawText("move ball with mouse and click mouse button to change color", 10, 10, 20, DARKGRAY);
+            DrawText("Press 'H' to toggle cursor visiblity", 10, 30, 20, DARKGRAY);
+
+            if(IsCursorHidden()) DrawText("CURSOR Hidden", 20, 60, 20, RED);
+            else DrawText("CURSOR VISIBLE", 20, 60, 20, LIME);
         EndDrawing();
     }
     CloseWindow();
